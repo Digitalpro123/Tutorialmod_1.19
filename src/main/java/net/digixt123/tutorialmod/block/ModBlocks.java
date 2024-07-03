271e7302bea7ef2ae414e88e52f0df7a8560d6a4
miriro <miriro@alumni.uv.es>
2024-07-03 15:48:59 +0200

    Tutorial 3 y 4 completos :)

--- src/main/java/net/digixt123/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/digixt123/tutorialmod/block/ModBlocks.java
@@ -39,6 +39,16 @@ public class ModBlocks {
                     .requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
 
+    public static final RegistryObject<Block> NETHERRACK_ZIRCON_ORE = registerBlock("netherrack_zircon_ore",
+            ()-> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f)
+                    .requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
+
+    public static final RegistryObject<Block> ENDSTONE_ZIRCON_ORE = registerBlock("endstone_zircon_ore",
+            ()-> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f)
+                    .requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
+
 
 
 
